843104fe7f450879dd6f3d8342c37a1cc161b031
Krish73529 <kb1521605@gmail.com>
2026-01-21 13:31:12 +0545

    node js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const msg = require("./index.js");
-console.log(msg);
+// const msg = require("./index.js");
+// console.log(msg);
+
+// Import the http module
+const http = require("http");
+
+// Create a server
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.end("Hello from server");
+});
+
+// Listen on port 3000
+server.listen(8000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
